58bbf612c58fe4be7011cb1c3cdfab00ab8c458c
Woojin Song <woojinsong93@gmail.com>
2025-03-08 17:00:43 -0800

    Fix logged out footer and add its separate js

---
+++ scripts/logged_out_footer.js
@@ -0,0 +1,14 @@
+// Home button handler
+document.getElementById("btn-home").addEventListener("click", function () {
+    window.location.href = "homepage1.html";
+});
+
+// Signup button handler
+document.getElementById("btn-signup").addEventListener("click", function () {
+    window.location.href = "signup.html";
+});
+
+// Login button handler
+document.getElementById("btn-login").addEventListener("click", function () {
+    window.location.href = "signin.html";
+});
