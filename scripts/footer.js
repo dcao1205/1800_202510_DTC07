f8cc6fd6ecb8a7e34ae007b3ada242962eda2878
Woojin Song <woojinsong93@gmail.com>
2025-03-22 17:50:53 -0700

    Adjust footer.js and make other pages consistent in style

--- scripts/footer.js
+++ scripts/footer.js
@@ -3,74 +3,70 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (footer) {
         footer.innerHTML = `
- <div class="my-0">
-    <section class="">
-        <footer class="text-center text-white" style="background-color: #727980;">
-            <nav class="container p-3">
-                <section class="d-flex justify-content-between">
-                    <!-- Home (Profile page) button -->
-                    <div id="home" class="d-flex flex-column align-items-center">
-                        <a id="btn-home" href="homepage2.html"
-                            class="text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor"
-                                class="bi bi-house-fill" viewBox="0 0 16 16">
-                                <path
-                                    d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
-                                <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
-                            </svg>
-                        </a>
-                    </div>
-                    <!-- My Listings button -->
-                    <div id="myListings" class="d-flex flex-column align-items-center">
-                        <a id="btn-home" href="#" class="text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="38"
-                                height="38" stroke-width="2">
-                                <path d="M9 6l11 0"></path>
-                                <path d="M9 12l11 0"></path>
-                                <path d="M9 18l11 0"></path>
-                                <path d="M5 6l0 .01"></path>
-                                <path d="M5 12l0 .01"></path>
-                                <path d="M5 18l0 .01"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <!-- Add listing button -->
-                    <div id="create-listing" class="d-flex flex-column align-items-center">
-                        <a id="btn-add" href="create_listing.html"
-                            class="text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="38"
-                                height="38" stroke-width="2">
-                                <path d="M12.5 21h-7.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5">
-                                </path>
-                                <path d="M3 10h18"></path>
-                                <path d="M10 3v18"></path>
-                                <path d="M16 19h6"></path>
-                                <path d="M19 16v6"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <!-- Notification button -->
-                    <div id="notification-bell" class="d-flex flex-column align-items-center">
-                        <button id="btn-bell" type="button" style="padding:0; border:none; background:none;"
-                            class="text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor"
-                                class="bi bi-bell-fill" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
-                            </svg>
-                        </button>
-                    </div>
-                </section>
-            </nav>
-            <!-- Copyright -->
-            <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
-                © 2025 Copyright: TextLibre
-            </div>
-        </footer>
-    </section>
-</div>
+    <footer class="text-center text-white mt-auto" style="background-color: #727980;">
+        <nav class="container p-3">
+            <section class="d-flex justify-content-between">
+                <!-- Home (Profile page) button -->
+                <div id="home" class="d-flex flex-column align-items-center">
+                    <a id="btn-home" href="homepage2.html"
+                        class="text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor"
+                            class="bi bi-house-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+                            <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+                        </svg>
+                    </a>
+                </div>
+                <!-- My Listings button -->
+                <div id="myListings" class="d-flex flex-column align-items-center">
+                    <a id="btn-home" href="#" class="text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="38"
+                            height="38" stroke-width="2">
+                            <path d="M9 6l11 0"></path>
+                            <path d="M9 12l11 0"></path>
+                            <path d="M9 18l11 0"></path>
+                            <path d="M5 6l0 .01"></path>
+                            <path d="M5 12l0 .01"></path>
+                            <path d="M5 18l0 .01"></path>
+                        </svg>
+                    </a>
+                </div>
+                <!-- Add listing button -->
+                <div id="create-listing" class="d-flex flex-column align-items-center">
+                    <a id="btn-add" href="create_listing.html"
+                        class="text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="38"
+                            height="38" stroke-width="2">
+                            <path d="M12.5 21h-7.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5">
+                            </path>
+                            <path d="M3 10h18"></path>
+                            <path d="M10 3v18"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </a>
+                </div>
+                <!-- Notification button -->
+                <div id="notification-bell" class="d-flex flex-column align-items-center">
+                    <button id="btn-bell" type="button" style="padding:0; border:none; background:none;"
+                        class="text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor"
+                            class="bi bi-bell-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
+                        </svg>
+                    </button>
+                </div>
+            </section>
+        </nav>
+        <!-- Copyright -->
+        <div class="text-center p-2 mb-0" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2025 Copyright: TextLibre
+        </div>
+    </footer>
 
 <div class="modal fade" id="notificationModal" tabindex="-1" aria-labelledby="notificationModalLabel"
     aria-hidden="true">
