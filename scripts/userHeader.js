6b1415b6fae8a1f37f5bab98113728dc953ca6d3
Woojin Song <wsong31@my.bcit.ca>
2025-04-01 15:42:15 -0700

    Edit header for user homepage

---
+++ scripts/userHeader.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('./templates/userHeader.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('loggedin-header-container').innerHTML = data;
+        // Attach event listeners after the footer is loaded
+        attachLogoutEventListener();
+      });
+
+      function attachLogoutEventListener() {
+        const logoutButton = document.querySelector('.nav-link[href="#logout"]'); 
+
+        if (logoutButton) {
+            logoutButton.addEventListener('click', function(event) {
+                event.preventDefault(); // Prevent default anchor behavior
+
+                firebase.auth().signOut().then(() => {
+                    console.log("User logged out successfully.");
+                    window.location.href = "index.html"; // Redirect to login page
+                }).catch((error) => {
+                    console.error("Logout Error:", error);
+                });
+            });
+        }
+    }
+});
